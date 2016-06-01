1c278cff873fdfcf931b1ab77a9e4ef2fe3f55c3
Brando <brando90@mit.edu>
2016-06-01 17:57:16 -0400

    two files Ive worked on for lerarning MatConvNet

---
+++ practical-cnn-2015a/setup_brando.m
@@ -0,0 +1,7 @@
+function setup_brando( )
+% setup_brando - make sure things are set up for using the exercises
+%run matconvnet/matlab/vl_setupnn
+run ../../matconvnet-1.0-beta20/matlab/vl_setupnn
+run vlfeat/toolbox/vl_setup.m
+
+end
