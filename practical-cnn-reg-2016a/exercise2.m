0780686c880ef791720ed9fb7b0adec9c935a208
Brando <brando90@mit.edu>
2016-06-06 00:51:07 -0400

    some more comments

--- practical-cnn-reg-2016a/exercise2.m
+++ practical-cnn-reg-2016a/exercise2.m
@@ -2,7 +2,7 @@ setup() ;
 
 %% Part 2.1:  Backward mode verification
 
-% Create a random input image batch
+% Create a random input image batch (M = 2)
 x = randn(10, 10, 1, 2, 'single') ;
 
 % Define a filter
@@ -12,10 +12,12 @@ w = single([
   0 -1 0]) ;
 
 % Forward mode: evaluate the convolution
-y = vl_nnconv(x, w, []) ;
+y = vl_nnconv(x, w, []) ; % (8 x 8 x 1 x 2) = (H_x - H_w + 1 x W_x - W_w + 1 x C x M)
 
 % Pick a random projection tensor
-p = randn(size(y), 'single') ;
+p = randn(size(y), 'single') ; % (8 x 8 x 1 x 2)
+p = zeros(size(y), 'single') ;
+p(1) = 1 ;
 
 % Backward mode: projected derivatives
 [dx,dw] = vl_nnconv(x, w, [], p) ; % <p, f(x;w)> = proj_p(f(x;w))
@@ -44,7 +46,7 @@ dy = vl_nnrelu(z, p) ;
 % Check the derivative numerically
 figure(22) ; clf('reset') ;
 set(gcf, 'name', 'Part 2.2: two layers backrpop') ;
-func = @(x) proj(p, vl_nnrelu(vl_nnconv(x, w, []))) ;
+func = @(x) proj(p, vl_nnrelu( vl_nnconv(x, w, [])) ) ;
 checkDerivativeNumerically(func, x, dx) ;
 
 %% Part 2.3: Design and verify your own layer
