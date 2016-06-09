b7bf4c3f706adbd5a7a96fca0f600c3ed96d3831
Brando <brando90@mit.edu>
2016-06-09 16:16:53 -0400

    trying to train model

--- practical-cnn-reg-2016a/1D_MatConvNet/hard_coded_test/main_1D_1layer_hard_coded_example.m
+++ practical-cnn-reg-2016a/1D_MatConvNet/hard_coded_test/main_1D_1layer_hard_coded_example.m
@@ -1,10 +1,16 @@
 clc;clear;clc;clear;
 %% prepare Data
-M = 3;
-x = zeros(1,1,1,M); % (1 1 1 2) = (1 1 1 M)
+M = 6;
+X = zeros(1,1,1,M); % (1 1 1 2) = (1 1 1 M)
 for m=1:M,
-    x(:,:,:,m) = m;
+    X(:,:,:,m) = m;
 end
+split = ones(1,M);
+split(floor(M*0.75):end) = 2;
+imdb.images.data = X;
+%imdb.images.label = squeeze( X );
+imdb.images.label = X;
+imdb.images.set = split;
 %%
 L1=3;
 w1 = randn(1,1,1,L1);
@@ -15,7 +21,7 @@ b2 = randn(1,L1);
 net.layers = {} ;
 net.layers{end+1} = struct('type', 'conv', ...
                            'name', 'conv1', ...
-                           'weights', {{w1, []}}, ...
+                           'weights', {{w1, b1}}, ...
                            'pad', 0) ;
 net.layers{end+1} = struct('type', 'bnorm', ...
                            'weights', {{ones(1,1,1,L1), zeros(1,1,1,L1)}}, ...
@@ -25,7 +31,7 @@ net.layers{end+1} = struct('type', 'relu', ...
                            'name', 'relu1' ) ;
 net.layers{end+1} = struct('type', 'conv', ...
                            'name', 'conv1', ...
-                           'weights', {{w2, []}}, ...
+                           'weights', {{w2, b2}}, ...
                            'pad', 0) ;
 net.layers{end+1} = struct('type', 'pdist', ...
                            'name', 'averageing1', ...
@@ -35,7 +41,7 @@ net.layers{end+1} = struct('type', 'pdist', ...
 fwfun = @l2LossForward;
 bwfun = @l2LossBackward;
 net = addCustomLossLayer(net, fwfun, bwfun) ;
-net = vl_simplenn_tidy(net)
+net = vl_simplenn_tidy(net);
 %% Train params
 trainOpts.expDir = 'results/' ;
 trainOpts.gpus = [] ;
@@ -45,5 +51,5 @@ trainOpts.plotDiagnostics = false ;
 %trainOpts.plotDiagnostics = true ; % Uncomment to plot diagnostics
 trainOpts.numEpochs = 20 ;
 trainOpts.errorFunction = 'none' ;
-
+%% CNN TRAIN
 net = cnn_train(net, imdb, @getBatch, trainOpts) ;
